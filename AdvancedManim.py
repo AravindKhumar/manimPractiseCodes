9f1a783da994d677452a4fbad26b4e4b825393ba
AravindKhumar <aravind199833@gmail.com>
2020-05-15 21:01:47 +0530

    Added how to use congif for a class

---
+++ AdvancedManim.py
@@ -0,0 +1,25 @@
+from manimlib.imports import *
+import numpy as np
+
+class usingConfig(Scene):
+	CONFIG = {
+
+		"text1" : TexMobject("\\int_{a}^{b} f(x) = f(b) - f(a)"),
+		"object1" : Circle().set_color(BLUE),
+		"scaleFactor" : 4,
+		"vector" : np.array([1,1,0])
+	}
+	def construct(self):
+
+		self.play(Write(self.text1))
+		self.wait()
+
+		self.play(self.text1.scale, self.scaleFactor)
+		self.wait()
+
+		self.play(ReplacementTransform(self.text1, self.object1))
+		self.wait()
+
+		self.play(self.object1.shift, self.vector)
+
+		self.wait()
